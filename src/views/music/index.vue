bdfecb8df7494b76e2a54c028df7fcd249164b18
jiangcong <1247590476qq.com>
2020-09-01 23:37:44 +0800

    播放器修改

---
+++ src/views/music/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="home-box">
+        <div class="music-div">
+            <music-player></music-player>
+        </div>
+    </div>
+</template>
+
+<script>
+import MusicPlayer from "../../components/musicPlayer/index";
+export default {
+  name: "music",
+  components: {
+    MusicPlayer
+  },
+  mounted() {
+    document.body.style.backgroundColor = "#ffffff";
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.music-div {
+  bottom: 4px;
+  position: absolute;
+}
+.home-box {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  background-color: #1d1d1d;
+}
+</style>
+
